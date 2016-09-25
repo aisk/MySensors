b21a71b0925416b65613a00bebf1331234b99df1
asaka <aisk1988@gmail.com>
2016-09-25 14:34:22 +0800

    Initial Commit

---
+++ MySensors/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MySensors
+//
+//  Created by 安龙 on 25/09/2016.
+//  Copyright © 2016 asaka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
