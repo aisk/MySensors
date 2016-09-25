c823e5e379d365d542185175ea706117015767c4
asaka <aisk1988@gmail.com>
2016-09-25 21:40:14 +0800

    most feature

--- MySensors/AppDelegate.swift
+++ MySensors/AppDelegate.swift
@@ -9,19 +9,96 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    var statusItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        do {
+            try SMCKit.open()
+        } catch {
+            let alert = NSAlert()
+            alert.messageText = "initailize SMC failed!"
+            alert.runModal()
+            NSApplication.shared().terminate(self)
+        }
+        
+        statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+        let icon = NSImage(named: "ic_memory")
+        icon?.isTemplate = true
+        statusItem.image = icon
+        statusItem.menu = NSMenu()
+        statusItem.menu?.delegate = self
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        let _ = SMCKit.close()
     }
+    
+    func onQuitMenuItemClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    func menuWillOpen(_ menu: NSMenu) {
+        let sensorsData = getSensorsData()
+        for data in sensorsData {
+            if data.items.count == 0 {
+                continue
+            }
+            let m = NSMenuItem()
+            m.title = "\(data.title):"
+            menu.addItem(m)
+            for item in data.items {
+                let m = NSMenuItem()
+                m.title = "\(item.0)  \(item.1)"
+                menu.addItem(m)
+            }
+            menu.addItem(NSMenuItem.separator())
+        }
 
-
+        let m = NSMenuItem()
+        m.title = "Quit"
+        m.action = #selector(onQuitMenuItemClicked)
+        menu.addItem(m)
+    }
+    
+    func getSensorsData() -> Array<SensorsData> {
+        var result: Array<SensorsData> = []
+        
+        do {
+            let fans = try SMCKit.allFans()
+            var items: Array<(String, String)> = []
+            for fan in fans {
+                items.append((fan.name, String(try SMCKit.fanCurrentSpeed(fan.id))))
+            }
+            result.append(SensorsData(title: "Fan", items: items))
+        } catch {
+            // pass
+        }
+        
+        do {
+            let temps = try SMCKit.allKnownTemperatureSensors()
+            var items: Array<(String, String)> = []
+            
+            for temp in temps {
+                items.append((temp.name, "\(try SMCKit.temperature(temp.code)) °C"))
+            }
+            items = items.sorted(by: {$0.0 < $1.0})
+            result.append(SensorsData(title: "Temperature", items: items))
+        } catch {
+            // pass
+        }
+        
+        return result
+    }
+    
+    func menuDidClose(_ menu: NSMenu) {
+        menu.removeAllItems()
+    }
 }
 
+struct SensorsData {
+    var title: String
+    var items: Array<(String, String)>
+}
